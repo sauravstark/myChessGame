58a0b93737928e37704de62039c8ed4bf3d79608
hacktivist1512 <48756156+hacktivist1512@users.noreply.github.com>
2019-04-10 01:39:58 +0530

    Update GameRules.h
    Redesigned the object oriented model for the basic rules of chess

--- Source Files/GameRules.h
+++ Source Files/GameRules.h
@@ -1,69 +1,100 @@
 #pragma once
 
-#include<vector>
+#include <vector>
+#include <tuple>
+#include <stack>
 
-class ChessBoard {
-	enum PieceColor {black, white};
+namespace chess {
+	enum Color { Black, White };
+	enum Type { King, Queen, Rook, Bishop, Knight, Pawn };
 
-	enum PieceType {king, queen, rook, knight, bishop, pawn};
+	class GameManager {
+		class Piece {
+		protected:
+			Type piece_type;
+			Color piece_color;
+			std::vector<std::tuple<int, int>> possible_directions;
+		public:
+			Piece(Type piece_type, Color piece_color);
+			Type getType();
+			Color getColor();
+			virtual bool can_jump_blocks() = 0;
+			virtual std::vector<std::tuple<int, int>> getMoveDirections() = 0;
+		};
+		class King : public Piece {
+		public:
+			King(Color piece_color);
+			bool can_jump_blocks();
+			std::vector<std::tuple<int, int>> getMoveDirections();
+		};
+		class Queen : public Piece {
+		public:
+			Queen(Color piece_color);
+			bool can_jump_blocks();
+			std::vector<std::tuple<int, int>> getMoveDirections();
+		};
+		class Rook : public Piece {
+		public:
+			Rook(Color piece_color);
+			bool can_jump_blocks();
+			std::vector<std::tuple<int, int>> getMoveDirections();
+		};
+		class Bishop : public Piece {
+		public:
+			Bishop(Color piece_color);
+			bool can_jump_blocks();
+			std::vector<std::tuple<int, int>> getMoveDirections();
+		};
+		class Knight : public Piece {
+		public:
+			Knight(Color piece_color);
+			bool can_jump_blocks();
+			std::vector<std::tuple<int, int>> getMoveDirections();
+		};
+		class Pawn : public Piece {
+		public:
+			Pawn(Color piece_color);
+			bool can_jump_blocks();
+			std::vector<std::tuple<int, int>> getMoveDirections();
+		};
+		class Move {
+			std::tuple <int, int> src, des;
+			Piece *moving_piece, *captured_piece;
+		public:
+			Move();
+			Move(std::tuple<int, int> src, std::tuple<int, int> des, Piece *moving_piece, Piece *captured_piece);
+			bool isInvalid();
+			std::tuple<int, int> getSrc();
+			std::tuple<int, int> getDes();
+			Piece* getMovingPiece();
+			Piece* getCapturedPiece();
+		};
+		class Board {
+		private:
+			std::vector<Piece*> all_pieces;
+			class Square {
+			private:
+				Piece* piece_placed;
+				friend class Board;
+			};
+			Square squares[8][8];
+			std::vector<std::tuple<int, int>> getValidMoves(std::tuple<int, int> block);
+		public:
+			Board();
+			Move move(std::tuple<int, int> src, std::tuple<int, int> des);
+			void display();
 
-	struct Cord {
-		int row, col;
-	};
+			friend class GameManager;
+		};
 
-	class Square {
-	private:
-		bool isOccupied;
-		PieceColor color;
+		Board board;
+		Color turn;
+		std::stack<Move> previous_moves;
+
 	public:
-		void place_piece();
-		void remove_piece();
+		GameManager();
+		void display();
+		void getMoves(std::tuple<int, int> block);
+		void move(std::tuple<int, int> src, std::tuple<int, int> des);
 	};
-
-	class Piece {
-		PieceColor color;
-		bool isCaptured;
-		Cord pos;
-		PieceType type;
-		virtual std::vector<Cord> getMoves() = 0;
-		virtual void move(Cord dest);
-		bool getCheckStatus(Cord dest);
-	};
-
-	class King : public Piece {
-		std::vector<Cord> getMoves();
-	};
-
-	class Queen : public Piece {
-		std::vector<Cord> getMoves();
-	};
-
-	class Rook : public Piece {
-		std::vector<Cord> getMoves();
-	};
-
-	class Knight : public Piece {
-		std::vector<Cord> getMoves();
-	};
-
-	class Bishop : public Piece {
-		std::vector<Cord> getMoves();
-	};
-
-	class Pawn : public Piece {
-		std::vector<Cord> getMoves();
-	};
-
-	class PieceSet {
-		PieceColor color;
-		King king;
-		Queen queen;
-		Rook rooks[2];
-		Knight knights[2];
-		Bishop bishops[2];
-		Pawn pawns[8];
-	};
-
-	Square board_squares[8][8];
-	PieceSet player_black, player_white;
-};
+}
