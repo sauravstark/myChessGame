42747b8642fab4f0b46e8cd6a8f1accb9c748f48
sereneshack <40608447+sereneshack@users.noreply.github.com>
2019-04-28 00:43:07 +0530

    Create Board.h

---
+++ Source Files/Board.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Pieces.h"
+
+namespace chess {
+	class Board {
+	private:
+		class Square {
+		private:
+			Piece* piece_placed;
+		public:
+			Piece* getPiece();
+			void placePiece(Piece *piece);
+			void removePiece();
+		};
+		Square squares[8][8];
+	public:
+		Board();
+		Piece* getPiece(int rank, int file);
+		void placePiece(Piece *piece, int rank, int file);
+		void removePiece(int rank, int file);
+		void display();
+	};
+}
