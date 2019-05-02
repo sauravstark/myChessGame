c9f5a5f97795fa8fae098a4a5a1b6606e1d53722
sereneshack <40608447+sereneshack@users.noreply.github.com>
2019-05-02 19:58:21 +0530

    Update GameRules.h

--- Source Files/GameRules.h
+++ Source Files/GameRules.h
@@ -6,7 +6,7 @@
 namespace chess {
 
 	class GameManager {
-		
+		enum GameState { selecting_square, selecting_target, game_over };
 		class Move {
 			std::tuple <int, int> src, des;
 			Piece *moving_piece, *captured_piece;
@@ -23,10 +23,19 @@ namespace chess {
 		Color turn;
 		Player team_white, team_black;
 		std::stack<Move> previous_moves;
-		void MakeMove(std::tuple<int, int> src, std::tuple<int, int> des);
-		bool game_over;
+		std::tuple<int, int> selector;
+		std::vector<std::tuple<int, int>> highlighted_squares;
+		void makeMove(std::tuple<int, int> src, std::tuple<int, int> des);
+		void undoMove();
+		GameState current_state;
 	public:
+		void setSelector(int rank, int file);
+		std::tuple<int, int> getSelector();
+		void setHighlightedSquares(int rank, int file);
+		void clearHighlightedSquares();
+		std::vector<std::tuple<int, int>> getHighlightedSquares();
 		GameManager();
 		void start();
+		void render();
 	};
 }
