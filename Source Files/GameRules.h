32c0c0c404bd67011853b7c8a6d779ec9e025aa9
koshi09 <48270314+koshi09@users.noreply.github.com>
2019-05-06 20:15:29 +0530

    Update GameRules.h

--- Source Files/GameRules.h
+++ Source Files/GameRules.h
@@ -1,24 +1,12 @@
 #pragma once
 
 #include <stack>
-#include "Players.h"
+#include "BackTrack.h"
 
 namespace chess {
 
 	class GameManager {
 		enum GameState { selecting_square, selecting_target, game_over };
-		class Move {
-			std::tuple <int, int> src, des;
-			Piece *moving_piece, *captured_piece;
-		public:
-			Move();
-			Move(std::tuple<int, int> src, std::tuple<int, int> des, Piece *moving_piece, Piece *captured_piece);
-			bool isInvalid();
-			std::tuple<int, int> getSrc();
-			std::tuple<int, int> getDes();
-			Piece* getMovingPiece();
-			Piece* getCapturedPiece();
-		};
 		Board board;
 		Color turn;
 		Player team_white, team_black;
